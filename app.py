3dbef98183bca6d8c12ac3882a078a2e29ccfad9
Rory James <rory.allen.james@gmail.com>
2023-02-02 15:17:38 -0500

    Added app sidebar info

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
 import streamlit as st
 import pandas as pd
+
+st.set_page_config(layout="wide")
+
+
+
+
+
+
+
+
+st.sidebar.info(
+        """
+        This app was built by Rory James
+        
+        Data for this project was sourced from the [Nasdaq Data Link](https://data.nasdaq.com/search)
+        
+        [Click Here For The Project Source Code](https://github.com/RoryAJames/Analyzing-Inflation) 
+        
+        Feel free to connect with me:        
+        [GitHub](https://github.com/RoryAJames) | [LinkedIn](https://www.linkedin.com/in/rory-james-873493111/)
+        
+    """
+    )
